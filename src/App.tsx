076448dc43fe98dc27b883dddae60df0fce5a18f
Vishwas <vishwas.g5@gmail.com>
2021-02-15 16:23:18 +0530

    Add Counter to demo fast refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Counter } from './Counter'
 const App = (): JSX.Element => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
+      <Counter />
     </>
   )
 }
