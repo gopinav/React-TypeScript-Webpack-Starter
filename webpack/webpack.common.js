c308e9f7ed96ebb42f5d0a3ff968535dc7b940a5
Rohit <tprohit9@gmail.com>
2021-06-17 16:33:32 +0530

    added WebpackPwaManifest

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,8 @@
 const path = require("path");
+const package = require("../package.json");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: path.resolve(__dirname, "..", "./src/react/index.tsx"),
@@ -38,7 +41,38 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
+      title: package.name,
+      favicon: path.resolve(__dirname, "..", "./src/assets/icons/favicon.ico"),
+      meta: {
+        author: package.author,
+        description: package.description,
+        keyword: package.keyword,
+        "theme-color": package["theme-color"]
+      }
     }),
+    new WebpackPwaManifest({
+      name: package.name,
+      short_name: package.name,
+      description: package.description,
+      background_color: package["theme-color"],
+      orientation: "any",
+      theme_color: package["theme-color"],
+      fingerprints: false,
+      publicPath: "/",
+      icons: [
+        {
+          src: path.resolve(__dirname, "..", "src/assets/icons/logo512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          ios: true
+        },
+        {
+          src: path.resolve(__dirname, "..", "src/assets/icons/maskable512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: "maskable",
+          ios: true
+        }
+      ]
+    })
   ],
   stats: "errors-only",
 };
